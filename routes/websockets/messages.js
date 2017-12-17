f45364d4bfbd7d2af637514e020599e45bd7ed31
Joseph Hassell <josephh2018@gmail.com>
2017-12-17 16:41:22 -0600

    Fixed Logic Bug

--- routes/websockets/messages.js
+++ routes/websockets/messages.js
@@ -22,23 +22,3 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-var express = require('express');
-var router = express.Router();
-
-var accountsJS = require("../lib/accounts.js");
-
-/**
-* Creates a new user account  
-* @param {Object} req
-* @param {Object} req.body
-* @param {String} req.body.username - The username of the account.
-*/
-router.post('/', function(req, res, next) {
-  accountsJS.new({username: req.body.username}).then((resp) => {
-  	res.json(resp).status(201);
-  }).catch((err) => {
-  	return next(err)
-  })
-});
-
-module.exports = router;
